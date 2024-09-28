750b69f7d8d48dfa784f31a8b240554a73efc17b
jgs <12345@qq.com>
2024-09-28 19:46:49 +0800

    update 2024-09-28

---
+++ assets/vidstack-C-clE4br-CJ9j9zbJ.js
@@ -0,0 +1,1 @@
+import{bF as i,bc as t}from"./app-DLxUEbjT.js";class n{constructor(s){this.La=s}Xa(){i(this.ya)&&this.fg()}$(){t(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{n as R};
